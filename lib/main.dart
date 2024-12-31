dde8f3d4b20f236a1bdec0ed279f147c01ee73f3
annisa :b <annisaputriaprilia12@gmail.com>
2024-12-31 11:25:14 +0700

    page navigation and responsive page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart'; //untuk ambil random english words wordpair
+import 'package:english_words/english_words.dart'; //
+import 'package:flutter/rendering.dart';
 import 'package:provider/provider.dart';
 import 'my_flutter_app_icons.dart';
 
@@ -7,7 +8,7 @@ void main() {
   runApp(const MyApp()); //menjalankan the app
 }
 
-class MyApp extends StatelessWidget { //widget is an elemnt where we can put other elements
+class MyApp extends StatelessWidget { 
   const MyApp({super.key});
 
   @override
@@ -29,18 +30,12 @@ class MyApp extends StatelessWidget { //widget is an elemnt where we can put oth
 class MyAppState extends ChangeNotifier { //class untuk state management atau status aplikasi
   var current = WordPair.random();
 
-  //mtehod untuk ganti kata
   void getNext(){
     current = WordPair.random();
     notifyListeners(); //notifyListeners untuk notify widget lain ada perubahan status
   }
-  //MyAppState menjelaskan data yang diperlukan oleh aplikasi ini agar berjalan dengan baik. Saat ini, kode ini hanya berisi variabel tunggal dengan pasangan kata acak saat ini. Anda akan menambahkannya nanti.
-  // Class status memperluas ChangeNotifier, yang artinya kode ini dapat memberi tahu kode lain tentang perubahannya sendiri. Misalnya, jika pasangan kata saat ini berubah, beberapa widget dalam aplikasi perlu mengetahuinya.
-  // Status dibuat dan disediakan untuk seluruh aplikasi menggunakan ChangeNotifierProvider (lihat kode di atas pada MyApp). Hal ini memungkinkan widget mana pun pada aplikasi untuk mendapatkan status.
-
-  var favorites = <WordPair>[]; //buat list kosong untuk favorit, list pakai [], set pakai{} agar tidak ada duplikat
+  var favorites = <WordPair>[];
   void toggleFavorite(){
-    //jika katanya yang sama sudah ada di favorit, maka hapus. jika belum, tambah
     if (favorites.contains(current)) {
       favorites.remove(current);
     } else {
@@ -50,26 +45,82 @@ class MyAppState extends ChangeNotifier { //class untuk state management atau st
   }
 }
 
-class MyHomePage extends StatelessWidget {
+//ubah dari stateless widget menjadi stateful widget atau widget yang memiliki state
+class MyHomePage extends StatefulWidget {
+  @override
+  //class ini memperluas state agasr dapat mengelola nilainya sendiri/mengubah nilainya sendiri
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0; //ini navigasi widget kiri, inisialiasi 0
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>(); //ambil status app dan notifikasi perubahan agar selalu uptodate
-    var pair = appState.current; //simpan status app ke yang current which is word pair nya.
+    Widget page;
+    //nah dibagian page ini diatur, apakah kiri nya 0 maka kanan nya page GeneratorPage dan laibn lain. 
+    switch (selectedIndex) {
+      case 0:
+        page = GeneratorPage();
+        break;
+      case 1:
+        //Placeholder adalah widget yang menampilkan kotak abu-abu yang menunjukkan di mana widget lain akan ditempatkan. pengganti sementara saja.
+        page = Placeholder();
+        break;
+      default: 
+        throw UnimplementedError(('no widget for $selectedIndex'));
+    }
+    //refactori builder layout untuk agar dia responsif kalau pengguna ubah windows aplikasi. gunanya biar bsia diatur lebar dan tingginya
+   return LayoutBuilder(
+     builder: (context, constraints) {
+       return Scaffold(
+        body: Row(
+          children: [
+            SafeArea(child: NavigationRail(
+              extended: constraints.maxWidth >= 600, //jika ada ruang lebar lebih dari 600 maka akan ditampikan juga teks label nya atau di extended.
+              destinations: [
+                NavigationRailDestination(
+                  icon: Icon(MyFlutterApp.home),
+                  label: Text('Home'),
+                  ),
+                NavigationRailDestination(
+                  icon: Icon(MyFlutterApp.heart),
+                  label: Text('Favorites'),
+                  ),
+              ],
+              selectedIndex: selectedIndex,
+              onDestinationSelected: (value) {
+                setState(() { //mirip notidy listener, memastikan ui selalu di update, tau dia sekarang ada di state yang mana
+                  selectedIndex = value;
+                }); }
+              ),
+            ),
+            Expanded(child: Container(
+              color: Theme.of(context).colorScheme.primary,
+              child: page, //page yang diatur diatas sebagai anak dari kiri
+            ))
+          ],
+        )
+       );
+     }
+   );
+  }
+}
+
+
+//dipisah jadi 2 kelas yang berbeda, yang di kiri dan kanan
+class GeneratorPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current;
 
     return Scaffold(
-      body: Center( //body: column wrap with center refactor
-        child: Column( //ingat big card ada didalam column, jadi kalau mau buat ditengah tinggal buat aja:
-          mainAxisAlignment: MainAxisAlignment.center, //sumbu axis (y)
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, 
           children: [
             Text('A random shit idea:'),
-            bigCard(pair: pair), //ambil status app class, yang current which is word pair nya. 
-            //extreacted widget, was           
-            //Text(pair.asLowerCase), //ambil status app class, yang current which is word pair nya.
-          SizedBox(height: 20), //ngasih pemisah antara big card dan elevated button
-        
-
-          //nah kita mau buat tombol like juga didalam row yang sama dengan tombol next, sehingga eleveted button sebelumnya kita wrapped in a row.
-        
+            bigCard(pair: pair), 
             //button
             Row(
               mainAxisSize: MainAxisSize.min,
@@ -85,10 +136,9 @@ class MyHomePage extends StatelessWidget {
 
                 ElevatedButton.icon(
                   onPressed: (){
-                    appState.toggleFavorite(); //ngelihat kondisi apakah sudah di like
+                    appState.toggleFavorite();
                     }, 
-                  //menggunkana contains karena void tidak bisa return, dan if dengan itenary operator
-                  icon: appState.favorites.contains(appState.current) //apakah di list favorite sudah menyimpan yang current atau maksudnya sudah di like? jika sudah maka iconnya yang heart selain itu empty.
+                  icon: appState.favorites.contains(appState.current) 
                       ? Icon(MyFlutterApp.heart)
                       : Icon(MyFlutterApp.heart_empty),
                   label: Text('Favorite'),
@@ -102,17 +152,7 @@ class MyHomePage extends StatelessWidget {
   }
 }
 
-
-//Terakhir, ada MyHomePage, widget yang telah Anda modifikasi. Setiap baris bernomor di bawah memetakan ke komentar nomor baris pada kode di atas:
-
-// Setiap widget menentukan metode build() yang dipanggil secara otomatis setiap kali kondisi widget berubah agar widget selalu dalam kondisi terbaru.
-// MyHomePage melacak perubahan terhadap status aplikasi saat ini menggunakan metode watch.
-// Setiap metode build harus menampilkan widget atau (yang lebih umum) pohon widget bertingkat. Dalam hal ini, widget tingkat tertinggi adalah Scaffold. Anda tidak akan bekerja dengan Scaffold dalam codelab ini, tetapi ini adalah widget yang berguna dan dapat ditemukan di sebagian besar aplikasi Flutter di dunia nyata.
-// Column adalah salah satu widget tata letak paling dasar pada Flutter. Widget tata letak ini mengambil sejumlah turunan dan menempatkannya pada kolom dari atas ke bawah. Secara default, kolom menempatkan turunan-turunannya secara visual di bagian atas. Anda akan segera mengubah ini agar kolom terpusat di tengah.
-// Anda mengubah widget Text ini pada langkah pertama.
-// Widget Text kedua ini mengambil appState, dan mengakses satu-satunya anggota dari class tersebut, current (yang merupakan WordPair). WordPair menyediakan beberapa pengambil yang berguna, seperti asPascalCase atau asSnakeCase. Di sini, kita menggunakan asLowerCase, tetapi Anda dapat mengubah ini sekarang jika Anda lebih menyukai salah satu alternatif yang ada.
-// Perhatikan bagaimana kode Flutter banyak menggunakan koma di akhir. Koma ini tidak harus ada, karena children adalah anggota terakhir (dan juga satu-satunya) dari daftar parameter Column ini. Namun, menggunakan koma di akhir umumnya adalah ide yang bagus: koma di akhir membuat penambahan anggota menjadi mudah, dan koma di akhir juga berfungsi sebagai petunjuk bagi pemformat otomatis Dart untuk meletakkan baris baru. Untuk informasi lebih lanjut, lihat panduan Pemformatan kode.
-
+// ignore: camel_case_types
 class bigCard extends StatelessWidget {
   const bigCard({
     super.key,
@@ -126,9 +166,9 @@ class bigCard extends StatelessWidget {
 
     final theme = Theme.of(context); //diatur di MyApp
     final style = theme.textTheme.displayMedium!.copyWith( color: theme.colorScheme.onPrimary,
-    ); //tipe font displayMedium, copyWith untuk mengganti warna font
+    ); 
 
-    return Card( //wrap with widget reafctor dan ganti jadi card
+    return Card(
       elevation: 2,
       color: theme.colorScheme.primary,
        child: Padding(
@@ -136,29 +176,9 @@ class bigCard extends StatelessWidget {
         child: Text(
           pair.asLowerCase,
           style: style,
-          semanticsLabel: "${pair.first} ${pair.second}", //gunanya dipisah agar aksebilitas pembaca layar untuk pengguna buta bisa mengerti makna bukan terbuat kata baru
+          semanticsLabel: "${pair.first} ${pair.second}", 
         ),       
-      //   seperti diatas instead of   
-      //   child: Padding(
-      //   padding: const EdgeInsets.all(20.0),
-      //   child: Text(pair.asLowerCase, style: style), ),
        ),    
-    ); //Padding dengan refactor dan wrap dengan padding
-    //Catatan: Flutter menggunakan Komposisi, bukan Pewarisan, kapan pun tersedia. Di sini, padding tidak menjadi atribut dari Text, melainkan sebuah widget! (seperti container)
-    //Sehingga hasil akhir kode: Kode ini menggabungkan widget Padding, dan juga Text, dengan widget Card.
+    ); //
   }
-
-  //tentang style
-  //Dengan menggunakan theme.textTheme,, Anda mengakses tema font aplikasi. Class ini mencakup anggota seperti bodyMedium (untuk teks standar ukuran medium), caption (untuk teks dari gambar), atau headlineLarge (untuk judul berukuran besar).
-  // Properti displayMedium adalah gaya font besar yang dimaksudkan untuk teks tampilan. Kata tampilan digunakan dalam artian tipografi di sini, seperti pada jenis huruf tampilan. Dokumentasi untuk displayMedium menyatakan bahwa, "gaya tampilan ditujukan untuk teks yang penting dan singkat"—tepat dengan kasus penggunaan kita.
-  // Properti displayMedium tema secara teori dapat berupa null. Dart, bahasa pemrograman yang Anda gunakan untuk menulis aplikasi ini, aman dari null, sehingga bahasa pemrograman ini tidak akan mengizinkan Anda memanggil metode objek yang berpotensi null. Namun, dalam hal ini, Anda dapat menggunakan operator ! ("bang operator") untuk meyakinkan Dart bahwa Anda memahami tindakan Anda. (displayMedium pasti tidak null dalam kasus ini. Namun, alasan kami mengetahui hal ini berada di luar cakupan codelab ini.)
-  // Memanggil copyWith() pada displayMedium menampilkan salinan gaya teks dengan perubahan yang Anda tentukan. Dalam hal ini, Anda hanya mengubah warna teks.
-  // Untuk mendapatkan warna baru, Anda mengakses tema aplikasi sekali lagi. Properti onPrimary skema warna menentukan warna yang cocok digunakan untuk warna primer aplikasi.
-
-  //explore ini copyWith untuk atur style
-  //copyWith() memungkinkan Anda mengubah lebih banyak tentang gaya teks daripada hanya warna. Untuk mendapatkan daftar lengkap properti yang dapat Anda ubah, letakkan kursor di dalam tanda kurung copyWith(), lalu tekan Ctrl+Shift+Space (Win/Linux) atau Cmd+Shift+Space (Mac).
-
-  //also explore with:
-  //dapat juga mengubah lebih banyak tentang widget Card. Misalnya, memperbesar bayangan kartu dengan meningkatkan nilai parameter elevation.
-  // Coba bereksperimen dengan warna. Selain theme.colorScheme.primary, ada juga .secondary, .surface, dan berbagai pilihan lainnya. Semua warna ini memiliki onPrimary padanannya masing-masing.
 }
--- lib/my_flutter_app_icons.dart
+++ lib/my_flutter_app_icons.dart
@@ -27,4 +27,5 @@ class MyFlutterApp {
 
   static const IconData heart = IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData heart_empty = IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData home = IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
 }
